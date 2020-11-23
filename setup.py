5d209fb0495fdf9295bda178e085c0df7a8a41dd
Maxime Mouchet <max@maxmouchet.com>
2020-11-23 17:27:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="cperm",
-    version="0.1.1",
+    version="0.1.2",
     author="Maxime Mouchet",
     author_email="max@maxmouchet.com",
     description="Python wrapper for libcperm.",
